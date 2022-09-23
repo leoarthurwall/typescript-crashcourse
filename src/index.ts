efb4997153471925886fe6aea50ad93093548e87
leoarthurwall <leowall93@gmail.com>
2022-09-23 10:51:41 +0100

    created a tsconfig file: updated the target to es6. set the outDir to ./dist. created a src and dist folder and put the indes.ts file in src folder and deleted the js file, which will presumable be recreated when we write more ts and be put into the dist folder

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let id: number = 5
+id = '5'
+
+// MANUAL CHECK: run 'tsc index' (file name) in the terminal and you can see any errors within the file
+// AUTO CHECK:  run 'tsc --watch index' and errors will automatically be updated in terminal
+// SETUP TSC CONFIG: tsc --init
+
